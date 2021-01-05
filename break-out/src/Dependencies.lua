b81a162a5445ae6672afea2abf38ff4dbc7da6e5
Furkan Ay <furkan-ay@outlook.com>
2021-01-05 19:37:08 +0300

    added main menu to breakout

---
+++ break-out/src/Dependencies.lua
@@ -0,0 +1,10 @@
+push = require 'lib/push' 
+
+Class = require 'lib/class' 
+
+require 'src/constants'
+
+require 'src/StateMachine'
+
+require 'src/states/BaseState'
+require 'src/states/StartState'
---
+++ break-out/src/constants.lua
@@ -0,0 +1,7 @@
+-- size of actual window
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+-- size of virtual window
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
